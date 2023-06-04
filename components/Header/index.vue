ea05588d5fb6d0297125114a8d1cd485e17b0248
Daniel Chiang Guerrero <chiang_guerrero_daniel@hotmail.com>
2023-06-04 15:07:41 -0500

    added default layout

--- components/Header/index.vue
+++ components/Header/index.vue
@@ -3,31 +3,25 @@
 </style>
 
 <template>
-  <v-card>
-    <v-layout>
-      <v-app-bar theme="dark">
-        <template v-slot:prepend>
-          <v-app-bar-nav-icon></v-app-bar-nav-icon>
-        </template>
-        <v-app-bar-title>
-          <svg
-            width="135"
-            height="100"
-            viewBox="0 0 135 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <text y="20" fill="white" fill-opacity="0.9">torre</text>
-            <text x="47" y="20" fill="#CDDC39">.co</text>
-          </svg>
-        </v-app-bar-title>
-        <v-spacer></v-spacer>
+  <v-app-bar theme="dark">
+    <template v-slot:prepend>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </template>
+    <v-app-bar-title>
+      <svg
+        width="135"
+        height="100"
+        viewBox="0 0 135 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <text y="20" fill="white" fill-opacity="0.9">torre</text>
+        <text x="47" y="20" fill="#CDDC39">.co</text>
+      </svg>
+    </v-app-bar-title>
+    <v-spacer></v-spacer>
 
-        <SearchField />
+    <SearchField />
 
-        <v-btn class="text-torre-yellow navbar-text" rounded="xl"
-          >SIGN IN</v-btn
-        >
-      </v-app-bar>
-    </v-layout>
-  </v-card>
+    <v-btn class="text-torre-yellow navbar-text" rounded="xl">SIGN IN</v-btn>
+  </v-app-bar>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-layout>
+    <Header />
+    <v-main>
+      <NuxtPage />
+    </v-main>
+  </v-layout>
+</template>
